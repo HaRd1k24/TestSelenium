dd36655f5378c815a20317ec43a372fdc8fc19fb
Lev Trapeznikov <leva.trapeznikov@mail.ru>
2021-05-20 15:09:39 +0300

    refactorExplicitWait

--- src/test/java/JavaCoreTest.java
+++ src/test/java/JavaCoreTest.java
@@ -1,14 +1,7 @@
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import java.util.*;
 
 public class JavaCoreTest {
     static WebDriver driver;
--- src/test/java/TestYandex.java
+++ src/test/java/TestYandex.java
@@ -1,13 +1,8 @@
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.support.ui.*;
 
-import java.util.concurrent.TimeUnit;
 
 public class TestYandex {
     static final String url = "https://yandex.ru";
